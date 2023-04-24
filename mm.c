7430ee032a0e40ce73fbca07d42672644b380a6c
zhongzero <1144265105@qq.com>
2023-04-24 22:59:58 +0800

    test

--- mm.c
+++ mm.c
@@ -60,14 +60,14 @@
 /*是否使用segregated lists*/
 // #define SEGREGATE
 /*下面三选一*/
-#define FIRST_FIT__AND_INSERT_HEAD
-// #define FIRST_FIT__AND_INSERT_TAIL
+// #define FIRST_FIT__AND_INSERT_HEAD
+#define FIRST_FIT__AND_INSERT_TAIL
 // #define BEST_FIT
 
 #ifdef SEGREGATE
 #define MAXCHECKLISTNUM 3//设当前寻找的块大小在list(x),则表示最多检查到list(x+MAXCHECKLISTNUM) (开成11表示全部检查)
 #else
-#define MAXCHECKNUM 40//最多往后寻找的块个数
+#define MAXCHECKNUM 30//最多往后寻找的块个数
 #endif
 
 #define max(a,b) ((a)>(b)?(a):(b))
